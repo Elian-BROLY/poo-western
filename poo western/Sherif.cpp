383377d3218e4755a119c185b07db52e63af3c27
ebroly1 <ebroly1@numerique-educatif.fr>
2025-02-06 15:42:31 +0100

    dernieres modifs OK

--- poo western/Sherif.cpp
+++ poo western/Sherif.cpp
@@ -1,14 +1,14 @@
 #include "Sherif.h"
 #include "Brigand.h"
 
-Sherif::Sherif(string nom, string boissonFavorite)
-	:Cowboy(nom, boissonFavorite)
+Sherif::Sherif(string nom, string boissonFavorite, int nbBrigand)
+	:Cowboy(nom, boissonFavorite), nbBrigand(nbBrigand)
 {
 }
 
 void Sherif::sePresente()
 {
-	cout << "(" << nom << ") -- " << "Bonjour, je suis Sherif " << nom << " et j'aime le " << boissonFavorite << endl;
+	cout << "(" << nom << ") -- " << "Bonjour, je suis Sherif " << nom << " et j'aime le " << boissonFavorite << " et j'ai arrete " << nbBrigand << " brigants" << endl;
 }
 
 void Sherif::rechercher(Brigand& brigand)
@@ -18,10 +18,21 @@ void Sherif::rechercher(Brigand& brigand)
 
 void Sherif::emprisonne(Brigand& brigand)
 {
+	nbBrigand++;
 	brigand.seFaitEmprisonne(*this);
 }
 
 void Sherif::coffrer()
 {
 	cout << "(" << nom << ") -- " << "Au nom de la loi, je vous arrete !" << endl;
+}
+
+int Sherif::getNbBringandEmprisonne()
+{
+	return nbBrigand;
+}
+
+void Sherif::respect()
+{
+	cout << "(" << nom << ") -- " << "Recommencez et Respectez moi, On m'appelle Sherif " << getNom() << endl;
 }
--- poo western/Sherif.h
+++ poo western/Sherif.h
@@ -7,12 +7,14 @@ class Sherif :
 	public Cowboy
 {
 private:
-
+	int nbBrigand = 0;
 public:
-	Sherif(string nom = "", string boissonFavorite = "eau");
+	Sherif(string nom = "", string boissonFavorite = "eau", int nbBrigand = 0);
 	void sePresente();
 	void rechercher(Brigand& brigand);
 	void emprisonne(Brigand& brigand);
 	void coffrer();
+	int getNbBringandEmprisonne();
+	void respect();
 };
 
